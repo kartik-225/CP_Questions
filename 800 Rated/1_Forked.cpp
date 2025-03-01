5b235d11d484fd7ffed1b6684453985ad0acbe63
Bruce Wayne <themaszone@gmail.com>
2025-03-01 19:44:55 +0530

    first problem

---
+++ 800 Rated/1_Forked.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solve(int a , int b , int xq , int yq , int xk , int yk){
+	set<pair<int,int>> king_coordinates = {{xk + a , yk + b} , {xk - a , yk + b} , {xk + a , yk - b} , {xk - a , yk - b},
+										   									 {xk + b , yk + a} , {xk - b , yk + a} , {xk + b , yk - a} , {xk - b , yk - a}};	
+	set<pair<int,int>> queen_coordinates = {{xq + a , yq + b} , {xq - a , yq + b} , {xq + a , yq - b} , {xq - a , yq - b},
+										                     {xq + b , yq + a} , {xq - b , yq + a} , {xq + b , yq - a} , {xq - b , yq - a}};
+	
+	vector<pair<int,int>> v;
+	set_intersection(king_coordinates.begin() , king_coordinates.end() , queen_coordinates.begin() , queen_coordinates.end() , back_inserter(v));
+
+	return v.size();
+}
+
+int main(){
+
+	int t ; cin >> t;
+	int a , b , xq , yq , xk , yk;
+	while(t--){
+		cin >> a >> b >> xk >> yk >> xq >> yq;
+		cout << solve(a , b , xq , yq , xk , yk) << '\n';
+	}
+
+	return 0;
+}
